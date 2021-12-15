392d6bc7efb56ed2aee9bc258212ddb1ac4cbc39
theovesy <theovesy@hotmail.fr>
2021-12-15 13:19:37 +0100

    separation into js files

---
+++ meals.js
@@ -0,0 +1,7 @@
+const meals = [
+	{name: "Pates", weight: 1, max: 1},
+	{name: "Cordon Bleu", weight: 0.5, max: 1},
+	{name: "Carrotes", weight: 1, max: 4},
+	{name: "Riz", weight: 0.8, max: 2},
+	{name: "Ramen", weight: 0.2, max:1}
+	];
---
+++ script.js
@@ -0,0 +1,75 @@
+function test(max){
+		document.getElementById("testfield").innerHTML = '';
+		for (let i = 0; i<max; i++) {
+			var r = Math.floor(Math.random() * meals.length);
+			document.getElementById("testfield").value += meals[r].name + "<br>";
+		}
+}
+
+function max_menu(meal_list) {
+		var count = 0;
+		for (i in meal_list){
+				count+= meal_list[i].max;
+			}
+		return count;
+	}
+
+function count(list, target) {
+		var counter = 0;
+		for (i in list){
+				if (list[i] == target){
+						counter++;
+					}
+			}
+		return counter;
+	}
+
+function generate(n, meal_list){
+		max_n = max_menu(meal_list);
+		if (n > max_n) {
+				n = max_n;
+			}
+		var wlist = makelist(meal_list);
+		var menu = [];
+		for (let i=0; i < n; i++){
+				var max_meal = false;
+				do {
+						var r = Math.floor(Math.random() * wlist.length);
+						var meal_index = wlist[r];
+						var meal = meal_list[meal_index];
+						if (meal.max <= count(menu, meal_index)){
+								max_meal = true;
+						} else {
+								menu.push(wlist[r]);
+								max_meal = false;
+								}
+					} while (max_meal);
+			}
+		return menu;
+	}
+
+function makelist(list){
+		var newlist = [];
+		for (let i=0; i < list.length; i++){
+				for (let j=0; j < meals[i].weight*100; j++){
+					newlist.push(i);
+					}
+			}
+		return newlist;
+	}
+
+function printmenu(){
+		var nb_meals = document.getElementById("quantity").value;
+		var menu = generate(nb_meals, meals);
+		document.getElementById("menu").innerHTML = '';
+		for (let i = 0; i < menu.length; i++) {
+				var meal = meals[menu[i]];
+				document.getElementById("menu").innerHTML += meal.name + "<br>";
+			}
+	}
+
+function copymenu(){
+		var text = document.getElementById("menu").innerHTML;
+		var text_tocopy = text.replace(/<br>/g, "\r\n");
+		navigator.clipboard.writeText(text_tocopy);
+	}
